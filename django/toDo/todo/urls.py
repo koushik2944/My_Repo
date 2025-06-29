b1e087c14714f3c6905fabf7c6657a243ee133b9
koushik2944 <koushikvhegde@gmail.com>
2025-06-29 22:54:26 +0530

    Added Edit and Delete features.

--- django/toDo/todo/urls.py
+++ django/toDo/todo/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    # Add Task
     path('addTask/',views.addTask,name='addTask'),
+    # Mark as Done
     path('markas_done/<int:pk>',views.markasDone, name='markas_done'),
+    # Mark as Undone
     path('markas_undone/<int:pk>',views.markasUndone, name='markas_undone'),
+    # Edit Task
+    path('editTask/<int:pk>/',views.editTask, name='editTask'),
+    # Delete Task with id
+    path('deleteTask/<int:pk>', views.deleteTask, name='deleteTask'),
 ]
--- django/toDo/todo/views.py
+++ django/toDo/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from .models import Task
 
 # Create your views here.
@@ -17,4 +17,22 @@ def markasUndone(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.is_completed = False
     task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task' : get_task,
+        }
+        return render(request, 'edit_task.html',context)
+    
+def deleteTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
