7c1a86785ebfa981b38b7c505c52072195500ac3
koushik2944 <koushikvhegde@gmail.com>
2025-06-29 22:12:45 +0530

    Added Mark as undone button.

--- django/toDo/todo/urls.py
+++ django/toDo/todo/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
+    path('markas_done/<int:pk>',views.markasDone, name='markas_done'),
+    path('markas_undone/<int:pk>',views.markasUndone, name='markas_undone'),
 ]
--- django/toDo/todo/views.py
+++ django/toDo/todo/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from .models import Task
 
 # Create your views here.
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def markasDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def markasUndone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
